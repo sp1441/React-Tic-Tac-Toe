996436fadfd9276ecf477e94e2bac8585aed0776
Sydney Pogue <spogue1441@gmail.com>
2023-06-28 18:01:24 -1000

    initial commit to move all to github

---
+++ src/app/components/Square.js
@@ -0,0 +1,14 @@
+export default function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,27 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import FilterableProductTable from './components/filterableProductTable';
+import Square from './components/Square.js';
 
 
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
-];
 
-const MORE_PRODUCTS = [
-  { category: "Cars", price: "$29999", stocked: true, name: "Toyota" },
-  { category: "Cars", price: "$27999", stocked: true, name: "Tesla" },
-  { category: "Cars", price: "$4000", stocked: true, name: "Ford" },
-  { category: "Trucks", price: "$70000", stocked: true, name: "Ford" },
-  { category: "Trucks", price: "$78800", stocked: true, name: "Tesla" },
-  { category: "Trucks", price: "$56000", stocked: true, name: "Toyota" }
-]
+// const PRODUCTS = [
+//   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+//   { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+//   { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+//   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+//   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+//   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+// ];
 
-
-function Square({ value }) {
-  return <button className="square">{value}</button>;
-}
+// const MORE_PRODUCTS = [
+//   { category: "Cars", price: "$29999", stocked: true, name: "Toyota" },
+//   { category: "Cars", price: "$27999", stocked: true, name: "Tesla" },
+//   { category: "Cars", price: "$4000", stocked: true, name: "Ford" },
+//   { category: "Trucks", price: "$70000", stocked: true, name: "Ford" },
+//   { category: "Trucks", price: "$78800", stocked: true, name: "Tesla" },
+//   { category: "Trucks", price: "$56000", stocked: true, name: "Toyota" }
+// ]
 
 export default function Board() {
   return (
