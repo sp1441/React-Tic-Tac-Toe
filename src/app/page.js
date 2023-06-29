9d14add78b0790281154004e8e6d88339c57850d
Sydney Pogue <spogue1441@gmail.com>
2023-06-28 16:58:23 -1000

    added value prop to Square component and passed number values from Board component into rendered Square components

--- src/app/page.js
+++ src/app/page.js
@@ -22,8 +22,8 @@ const MORE_PRODUCTS = [
 ]
 
 
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
@@ -33,19 +33,19 @@ export default function Board() {
       <FilterableProductTable products={MORE_PRODUCTS} /> */}
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
 
     </>
