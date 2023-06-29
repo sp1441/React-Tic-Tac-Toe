cf216f0f541f80f2ca9b07be1011a46c45a9a33f
Sydney Pogue <spogue1441@gmail.com>
2023-06-28 16:41:31 -1000

    arranged squars into a board with numbers 1-9

--- src/app/page.js
+++ src/app/page.js
@@ -21,22 +21,28 @@ const MORE_PRODUCTS = [
   { category: "Trucks", price: "$56000", stocked: true, name: "Toyota" }
 ]
 
-export default function Home() {
+export default function Board() {
   return (
     <>
       {/* <FilterableProductTable products={PRODUCTS} />
       <FilterableProductTable products={MORE_PRODUCTS} /> */}
 
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
-      <button className={"square"}>X</button>
+      <div className="board-row">
+        <button className={"square"}>1</button>
+        <button className={"square"}>2</button>
+        <button className={"square"}>3</button>
+      </div>
+      <div className="board-row">
+        <button className={"square"}>4</button>
+        <button className={"square"}>5</button>
+        <button className={"square"}>6</button>
+      </div>
+      <div className="board-row">
+        <button className={"square"}>7</button>
+        <button className={"square"}>8</button>
+        <button className={"square"}>9</button>
+      </div>
 
     </>
-  )
+  );
 }
